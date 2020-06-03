4f90c5e8b0392e55048086597fc6076f1a9ac8a0
maxcohn <maxc4321@gmail.com>
2020-06-02 21:41:48 -0400

    chapter 2 finished

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+
+const IMAGE_WIDTH: usize = 256;
+const IMAGE_HEIGHT: usize = 256;
+
+fn main() {
+    println!("P3");
+    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
+    println!("255");
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprintln!("Scan lines left: {}", j);
+        for i in (0..IMAGE_WIDTH) {
+            let r = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let g = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
+            let b = 0.25;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+    
+
+}
