143c13564bbe5eb3a2050ec1f6a9adaa4e97906b
maxcohn <maxc4321@gmail.com>
2020-06-02 23:03:20 -0400

    Added Vec3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 
+
+mod vec3;
+use vec3::Vec3;
+
 const IMAGE_WIDTH: usize = 256;
 const IMAGE_HEIGHT: usize = 256;
 
@@ -9,7 +13,7 @@ fn main() {
 
     for j in (0..IMAGE_HEIGHT).rev() {
         eprintln!("Scan lines left: {}", j);
-        for i in (0..IMAGE_WIDTH) {
+        for i in 0..IMAGE_WIDTH {
             let r = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
             let g = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
             let b = 0.25;
@@ -20,6 +24,14 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+
+    let v = Vec3::from(5f64, -5f64, 0f64);
+    
+    let mut v2 = -v;
+
+    v2 += v;
+
+    println!("{}, {}, {}", v2[0], v2[1], v2[2]);
     
 
 }
---
+++ src/vec3.rs
@@ -0,0 +1,157 @@
+
+use std::ops;
+
+
+/// A collections of three points representing a location in 3D space.
+#[derive(Debug, Clone, Copy)]
+pub struct Vec3 {
+    e: [f64; 3],
+}
+
+
+impl Vec3 {
+
+    /// Create a new Vec3 with all values set to 0
+    pub fn new() -> Self {
+        Vec3 {
+            e: [0f64, 0f64, 0f64],
+        }
+    }
+
+    /// Create a new Vec3 with the given values
+    pub fn from(x: f64, y: f64, z: f64) -> Self {
+        Vec3 {
+            e: [x, y, z],
+        }
+    }
+
+    /// Get the X coordinate of the Vec3
+    #[inline]
+    pub fn x(&self) -> f64{
+        self.e[0]
+    }
+
+    /// Get the Y coordinate of the Vec3
+    #[inline]
+    pub fn y(&self) -> f64{
+        self.e[1]
+    }
+
+    /// Get the Z coordinate of the Vec3
+    #[inline]
+    pub fn z(&self) -> f64{
+        self.e[2]
+    }
+
+    #[inline]
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    #[inline]
+    pub fn length_squared(&self) -> f64 {
+        self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
+    }
+
+    pub fn print(&self) {
+        println!("{} {} {}", self.x(), self.y(), self.z());
+    }
+
+    #[inline]
+    pub fn dot(&self, other: Self) -> f64 {
+        self.e[0] * other.e[0] + self.e[1] * other.e[1] + self.e[2] + other.e[2]
+    }
+
+    #[inline]
+    pub fn cross(&self, other: Self) -> Self {
+        Self::from(
+            self.e[1] * other.e[2] - self.e[2] * other.e[1],
+            self.e[2] * other.e[0] - self.e[0] * other.e[2],
+            self.e[0] * other.e[1] - self.e[1] * other.e[0]
+        )
+    }
+
+    #[inline]
+    pub fn unit_vector(&self) -> Self {
+        *self / self.length()
+    }
+}
+
+impl ops::Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Self::Output {
+        Vec3 {
+            e: [-self.x(), -self.y(), -self.z()]
+        }
+    }
+}
+
+impl ops::Index<usize> for Vec3 {
+    type Output = f64;
+
+    fn index(&self, i: usize) -> &Self::Output {
+        &self.e[i]
+    }
+}
+
+impl ops::AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            e: [self.x() + other.x(), self.y() + other.y(), self.z() + other.z()]
+        }
+    }
+}
+
+impl ops::MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, other: f64) {
+        *self = Self {
+            e: [self.x() + other, self.y() + other, self.z() + other]
+        }
+    }
+}
+impl ops::DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, other: f64) {
+        *self *= 1f64 / other
+    }
+}
+
+impl ops::Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, other: Vec3) -> Self::Output {
+        Self::from(self.x() + other.x(), self.y() + other.y(), self.z() + other.z())
+    }
+}
+
+impl ops::Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, other: Vec3) -> Self::Output {
+        Self::from(self.x() - other.x(), self.y() - other.y(), self.z() - other.z())
+    }
+}
+
+impl ops::Mul for Vec3 {
+    type Output = Vec3;
+    fn mul(self, other: Vec3) -> Self::Output {
+        Self::from(self.x() * other.x(), self.y() * other.y(), self.z() * other.z())
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, t: f64) -> Self::Output {
+        Self::from(self.x() * t, self.y() * t, self.z() * t)
+    }
+}
+
+impl ops::Div<f64> for Vec3 {
+    type Output = Vec3;
+    fn div(self, t: f64) -> Self::Output {
+        self * (1f64 / t)
+    }
+}
+
+pub type Point3 = Vec3;
+pub type Color = Vec3;
